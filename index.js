51940f05f80aeac9fbf8029f4ed13803ebb8d803
Arijit <arijitchak15@gmail.com>
2023-10-19 01:55:05 +0530

    changes

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 const stripeInstance = stripe(process.env.STRIPE_SECRET);
 
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 // const USERNAME = process.env.DB_USERNAME;
 // const PASSWORD = process.env.DB_PASSWORD;
